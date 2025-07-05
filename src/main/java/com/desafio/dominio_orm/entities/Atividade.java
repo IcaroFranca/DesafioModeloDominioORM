e2770546ed0bebb0e1e343275e6f8cdf8e85d8b7
GodIcarus <38266289+IcaroFranca@users.noreply.github.com>
2025-07-05 07:36:23 -0300

    Participante

--- src/main/java/com/desafio/dominio_orm/entities/Atividade.java
+++ src/main/java/com/desafio/dominio_orm/entities/Atividade.java
@@ -1,11 +1,20 @@
 package com.desafio.dominio_orm.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "tb_atividade")
 public class Atividade {
 	
 	@Id
@@ -18,6 +27,12 @@ public class Atividade {
 	@ManyToOne
 	@JoinColumn(name = "categoria_id")
 	private Categoria categoria;
+	
+	@ManyToMany
+	@JoinTable(name = "tb_atividade_participante",
+	joinColumns = @JoinColumn(name = "atividade_id"),
+	inverseJoinColumns = @JoinColumn(name = "participante_id"))
+	private Set<Participante> participantes = new HashSet<>();
 	
 	public Atividade() {
 	}
@@ -60,5 +75,17 @@ public class Atividade {
 
 	public void setPreco(Double preco) {
 		this.preco = preco;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public Set<Participante> getParticipantes() {
+		return participantes;
 	}
 }
